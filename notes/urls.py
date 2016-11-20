25b6f65510a2f7b68f56febdc8d483bfce90837b
emadahmed97 <emad1997@gmail.com>
2016-11-20 10:44:58 -0500

    Add final touches

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'notes'
+
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<article_id>[0-99]+)/$', views.detail, name='detail')
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -8,3 +8,10 @@ def index(request):
     print latest_post_list
     context = {'latest_post_list':latest_post_list }
     return render(request, 'notes/index.html',context)
+
+def detail(request,article_id):
+    latest_post_list = Classes.objects.order_by('-title')[:10]
+    article = Notes.objects.get(pk=article_id)
+    print latest_post_list
+    context = {'latest_post_list':latest_post_list, 'article':article }
+    return render(request, 'notes/index.html',context)
